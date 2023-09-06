82451dfc219fac64b71fa7969a8dc5222901a80c
Matheus H <matheus.henrique4474@gmail.com>
2023-09-06 14:45:20 -0300

    i forget to add the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { LoginComponent } from './login/login.component';
-import { ParcelsComponent } from './user/parcels/parcels.component';
-import { DashboardComponent } from './user/dashboard/dashboard.component';
-import { VisitorsComponent } from './user/visitors/visitors.component';
-import { UsersComponent } from './admin/users/users.component';
-import { ApartsComponent } from './admin/aparts/aparts.component';
+
+import { EncriptService } from './services/encript.service';
+import { AuthService } from './services/auth.service';
+
+import { AdminParcelsComponent } from './admin/admin-parcels/admin-parcels.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { AdminVisitorsComponent } from './admin/admin-visitors/admin-visitors.component';
+import { AdminApartsComponent } from './admin/admin-aparts/admin-aparts.component';
+import { AdminResidentsComponent } from './admin/admin-residents/admin-residents.component';
+
+import { UserApartsComponent } from './user/user-aparts/user-aparts.component';
+import { UserVisitorsComponent } from './user/user-visitors/user-visitors.component';
+import { UserParcelsComponent } from './user/user-parcels/user-parcels.component';
+import { UserDashboardComponent } from './user/user-dashboard/user-dashboard.component';
+import { UserResidentsComponent } from './user/user-residents/user-residents.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ParcelsComponent,
-    DashboardComponent,
-    VisitorsComponent,
-    UsersComponent,
-    ApartsComponent,
+    AdminParcelsComponent,
+    AdminDashboardComponent,
+    AdminVisitorsComponent,
+    AdminApartsComponent,
+    AdminResidentsComponent,
+    UserApartsComponent,
+    UserVisitorsComponent,
+    UserParcelsComponent,
+    UserDashboardComponent,
+    UserResidentsComponent,
+    ErrorPageComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [EncriptService, AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
