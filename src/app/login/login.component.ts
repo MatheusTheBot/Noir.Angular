e9d3384981e7c023008266b47befbcf64db22752
Matheus H <matheus.henrique4474@gmail.com>
2023-09-13 15:11:32 -0300

    added login handler

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  handleLogin() {
+    let obj: Object = {
+      username: this.username,
+      password: this.password,
+    };
+
+    fetch('http://localhost:8080/login', {
+      method: 'POST',
+      body: JSON.stringify(obj),
+    }).then((data) => {
+      console.log(data);
+    });
+  }
 }
